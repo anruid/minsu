1729a0cbc4045b46caf3b861399e8c8e4da2d472
kk666 <hu2k99999>
2019-10-24 21:08:25 +0800

    1.02优化

---
+++ waterFall.php
@@ -0,0 +1,21 @@
+<?php
+$cb= $_GET['callback'];
+$uname = $_GET['username'];
+if($uname == '123'){
+    echo $cb.'('.'[{src:"img/12.png"},
+    {src:"img/11.png"},
+    {src:"img/1.png"},
+    {src:"img/14.png"},
+    {src:"img/5.png"},
+    {src:"img/13.png"},
+    {src:"img/4.png"},
+    {src:"img/7.png"},
+    {src:"img/6.png"},
+    {src:"img/3.png"},
+    {src:"img/2.png"},
+    {src:"img/9.png"},
+    {src:"img/8.png"}]'.')';
+}else{
+    echo $cb.'('."'hello'".')';
+}
+?>
